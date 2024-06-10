ba2c4b3023c0f35f403d5d465fb8927532579a2f
Andrés Corrada-Emmanuel <andres.corrada@dataengines.com>
2024-06-10 10:27:39 -0400

    Further enhacements to the plotting of binary classification performance.

--- python/src/ntqr/r2/plots.py
+++ python/src/ntqr/r2/plots.py
@@ -27,24 +27,44 @@ def prepare_accuracy_axes(axes, title):
 
     # Styling them
     axes.tick_params(
-        axis="both", which="major", grid_color="g", grid_linewidth=2
+        axis="both", which="major", grid_color="white", grid_linewidth=2
     )
     axes.tick_params(
         axis="both", which="minor", grid_color="k", grid_alpha=0.4
     )
 
     # Blindspot line
-    axes.plot([0, 100], [100, 0], color="r")
+    axes.plot([0, 100], [100, 0], color="black")
 
     # The labels for the four quadrants of the unit cube
     quad_label_info = [
         ("A", (75, 75)),
         ("C", (25, 25)),
-        ("B", (25, 75)),
-        ("D", (75, 25)),
+        ("$F_1$", (25, 75)),
+        ("$F_2$", (75, 25)),
     ]
     for quad_label, pos in quad_label_info:
-        axes.annotate(quad_label, pos, size=18, alpha=0.4)
+        axes.annotate(
+            quad_label,
+            pos,
+            size=48,
+            color="white",
+            alpha=0.5,
+            textcoords="offset points",
+            xytext=(-12, -12),
+        )
+
+    # Color the quadrants
+    # axes.axhspan(0, 0.5, facecolor="0.5")
+    red_color = "#FF8C8C"
+    nasa_blue = "#0B3D91"
+    light_blue = "#ADD8E6"  # "#E4F6F8"  # "#ADD8E6"
+    orange_color = "#F6B26B"
+    alpha = 1.0
+    axes.axvspan(50, 100, ymin=0.5, ymax=1, color=light_blue, alpha=alpha)
+    axes.axvspan(0, 50, ymin=0, ymax=0.5, color=orange_color, alpha=alpha)
+    axes.axhspan(50, 100, xmin=0, xmax=0.5, color=red_color, alpha=alpha)
+    axes.axhspan(0, 50, xmin=0.5, xmax=1, color=red_color, alpha=alpha)
 
     axes.set_xlabel(r"$P_{i,a}$", fontsize=12)
     axes.set_ylabel(r"$P_{i,b}$", fontsize=12)
@@ -55,7 +75,13 @@ def prepare_accuracy_axes(axes, title):
 
 
 def _plot_evaluations(
-    ax, evals, title="Title", legend_loc="best", withArrows=False
+    ax,
+    evals,
+    title="Title",
+    legend_loc="best",
+    withArrows=False,
+    classifier_labels=None,
+    horizontalalignment="left",
 ):
     prepare_accuracy_axes(ax, title)
 
@@ -64,10 +90,33 @@ def _plot_evaluations(
 
     texts_to_adjust = []
     for label, x, y, marker, colors, sizes in evals:
-        ax.scatter(x, y, s=sizes, c=colors, marker=marker, label=label)
+        ax.scatter(
+            x,
+            y,
+            s=sizes,
+            color=colors,
+            marker=marker,
+            label=label,
+            alpha=0.9,
+            clip_on=False,
+        )
         # Adding the classifier label to each point
         for i in range(len(x)):
-            texts_to_adjust.append(ax.annotate(str(i + 1), (x[i], y[i])))
+            if classifier_labels:
+                i_label = classifier_labels[i]
+            else:
+                i_label = str(i + 1)
+            texts_to_adjust.append(
+                ax.annotate(
+                    i_label,
+                    (x[i], y[i]),
+                    color="red",
+                    alpha=1.0,
+                    size="18",
+                    usetex=False,
+                    horizontalalignment=horizontalalignment,
+                )
+            )
 
     if withArrows:
         # Since we are comparing evaluations, both evaluations have
@@ -92,7 +141,7 @@ def _plot_evaluations(
             )
             ax.add_patch(arrow)
 
-    ax.legend(loc=legend_loc)
+    ax.legend(loc=legend_loc, facecolor="white", labelcolor="black")
 
 
 def compare_evaluations(
@@ -103,6 +152,7 @@ def compare_evaluations(
     legend_loc="best",
     withArrows=False,
     suptitle=None,
+    classifier_labels=None,
 ):
     fig, axs = plt.subplots(
         1, len(evals) - 1, figsize=figsize, layout="constrained"
@@ -119,6 +169,7 @@ def compare_evaluations(
             title=titles[i],
             legend_loc=legend_loc,
             withArrows=withArrows,
+            classifier_labels=classifier_labels,
         )
         j += 1
 
@@ -135,6 +186,7 @@ def plot_evaluations(
     figsize=(5, 4),
     legend_loc="lower left",
     withArrows=False,
+    classifier_labels=None,
 ):
     plt.style.use("_mpl-gallery")
 
@@ -142,7 +194,12 @@ def plot_evaluations(
     fig, ax = plt.subplots(figsize=figsize, layout="constrained")
 
     _plot_evaluations(
-        ax, evals, title=title, legend_loc=legend_loc, withArrows=withArrows
+        ax,
+        evals,
+        title=title,
+        legend_loc=legend_loc,
+        withArrows=withArrows,
+        classifier_labels=classifier_labels,
     )
 
     plt.show()
