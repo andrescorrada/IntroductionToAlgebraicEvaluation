c147f9945b4677044c7ecef144f7e9288b7603a9
Andrés Corrada-Emmanuel <andres.corrada@dataengines.com>
2024-06-10 10:29:14 -0400

    Moving tests over to the sympy.Rational instead of the fractio.Fraction class to provide greater interoperability between algebraic and numerical computations.

--- python/tests/test_r2_postulates.py
+++ python/tests/test_r2_postulates.py
@@ -1,7 +1,6 @@
 """@author: Andrés Corrada-Emmanuel."""
-from fractions import Fraction
-
 import pytest
+import sympy
 
 from ntqr.r2.datasketches import (
     TrioVoteCounts,
@@ -15,9 +14,9 @@ from ntqr.r2.examples import uciadult_label_counts
 @pytest.mark.parametrize(
     "label_accuracies, voting_frequencies",
     (
-        (1, Fraction(18432653, 1357332964)),
-        (1, Fraction(18272925, 1357332964)),
-        (1, Fraction(16803485, 1357332964)),
+        (1, sympy.Rational(18432653, 1357332964)),
+        (1, sympy.Rational(18272925, 1357332964)),
+        (1, sympy.Rational(16803485, 1357332964)),
     ),
 )
 def test_one_classifier_postulate(label_accuracies, voting_frequencies):
