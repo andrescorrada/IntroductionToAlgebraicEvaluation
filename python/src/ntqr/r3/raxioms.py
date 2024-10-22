eb879d1c5d54cdefc8e8825c80f0a0b32fc1e6c4
Andrés Corrada-Emmanuel <andres.corrada@dataengines.com>
2024-10-22 13:58:09 -0400

    The first commit related to R=3 tests

--- python/src/ntqr/r3/raxioms.py
+++ python/src/ntqr/r3/raxioms.py
@@ -1,15 +1,19 @@
-"""Postulates of evaluation for R=3 tests.
+"""Axioms of evaluation for R=3 tests.
 
 The number of decision patterns when 3 responses to a question are possible
-is 3^N, where N is the number of responders. As in the R=2 postulates case,
+is 3^N, where N is the number of responders. As in the R=2 axioms case,
 we can start with statistics of correctness in a test that only involve a
-single responder. We begin the build out of the R=3 postulates with the N=1
-postulates, in other words. Then we will proceed to the N=2 postulates, the
+single responder. We begin the build out of the R=3 axioms with the N=1
+axioms, in other words. Then we will proceed to the N=2 axioms, the
 N=3 postulates and so on.
 
 Although we suspect that the N=3, R=3 error independent model also has a
 closed algebraic solution, we have failed to get one using 128GB of RAM.
-But we are publishing the N=1 case now since it is solvable.
+But we are publishing the N=1 case now since it is solvable. In addition,
+it is possible to build logical alarms for misaligned classifiers just
+using the single classifier axioms. This is good news since the single
+classifier case axioms are easily constructed for any finite number of
+classes/responses.
 
 Variables
 ---------
@@ -31,15 +35,54 @@ terms of the two wrong choices. And this is a choice that is going to work
 for any R so it is the one we choose here.
 
 So, for example, assuming the three labels are given by (a, b, c). The 'a'
-label accuracy would expressed in terms of the two error rates as,
+label accuracy would be expressed in terms of the two error rates as,
 P_i_a_a = 1 - P_i_b_a - P_i_c_a
 """
 
 import sympy
 
-# The observable voting frequencies for the single 3-label classifier
-single_classifier_voting_frequencies = []
-for var_name in (r"f_a", r"f_b", r"f_c"):
-    globals()[var_name] = sympy.Symbol(var_name)
-    single_classifier_voting_frequencies.append(globals()[var_name])
-print(single_classifier_voting_frequencies)
+
+class SingleClassifierAxioms:
+    """
+    The axioms of a single R=3 classifier
+
+    ...
+
+    Attributes
+    ----------
+    responses : List[simpy.Symbol..]
+        The variables for the observed interger count of the three classes
+        ('a', 'b', 'c') in the test.
+
+        Rai : number of 'a' responses
+        Rbi : number of 'b' responses
+        Rci : number of 'c' responses
+
+    correctness_variables: List[simpy.Symbol..]
+        The variables associated with correct and wrong responses given
+        the true label.
+
+        The variables associated with true label 'a'
+        Raia : number of 'a' responses (also number of correct)
+        Rbia : number of 'b' responses
+        Rcia : number of 'c' responses
+
+        The variables associated with true label 'b'
+        Raib : number of 'a' responses
+        Rbib : number of 'b' responses  (also number of correct)
+        Rcib : number of 'c' responses
+
+        The variables associated with true label 'c'
+        Raic : number of 'a' responses
+        Rbic : number of 'b' responses
+        Rcic : number of 'c' responses (also number of correct)
+
+
+    Methods
+    -------
+    evaluate_axioms(eval_dict): List[expression, expression, expression]
+        Evaluates the axioms given the variable substitutions in 'eval_dict'.
+    satisfies_axioms(eval_dict): Boolean
+        Checks if the variable substitutions in 'eval_dict' make all three
+        axioms identically zero.
+    """
