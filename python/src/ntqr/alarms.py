0ce7ea66efdd80163e4b5b01c6f46d43fac6ab95
Andrés Corrada-Emmanuel <andres.corrada@dataengines.com>
2024-10-26 07:07:50 -0400

    Checking in generic 'alarms.py' code.

---
+++ python/src/ntqr/alarms.py
@@ -0,0 +1,78 @@
+"""Algorithms for logical alarms based on the axioms.
+
+Formal verification of unsupervised evaluations is carried out by
+using the agreements and disagreements between classifiers to detect
+if they are misaligned given a safety specification.
+"""
+
+
+class SingleClassifierAxiomAlarm:
+    """Alarm based on the single classifier axioms for the ensemble members
+
+    Initialize with 'classifiers_axioms', list of
+    ntqr.r3.raxioms.SingleClassifierAxioms, one per classifier we want
+    to compare.
+    """
+
+    def __init__(self, Q, classifiers_axioms, cls_single_evals):
+        """"""
+
+        self.Q = Q
+
+        self.classifiers_axioms = classifiers_axioms
+        self.labels = classifiers_axioms[0].labels
+
+        self.cls_single_evals = cls_single_evals
+
+    def generate_safety_specification(self, factors):
+        """Creates a safety specification function query given min label
+        accuracies."""
+
+        def satisfies_safety_specification(qs, correct_responses):
+            """Checks that list of label accuracies satisfy the
+            equation, factor*correct_response - ql > 0."""
+            tests = [
+                (factor * correct_response - ql) > 0
+                for factor, correct_response, ql in zip(
+                    factors, correct_responses, qs
+                )
+            ]
+            print("tests for safety specification: ", tests, " ", all(tests))
+            return all(tests)
+
+        return satisfies_safety_specification
+
+    def misaligned_at_qs(self, safety_spec, qs, responses):
+        """Boolean test to see if the classifiers violate the safety
+        specification at given questions correct number."""
+
+        assert self.check_responses(qs, responses)
+
+        sca_evals = [
+            self.cls_single_evals(self.Q, axioms)
+            for axioms in self.classifiers_axioms
+        ]
+        max_corrects = [
+            sca_eval.max_correct_at_qs(qs, cresponses)
+            for sca_eval, cresponses in zip(sca_evals, responses)
+        ]
+
+        return all(
+            [
+                (not safety_spec(qs, max_correct))
+                for max_correct in max_corrects
+            ]
+        )
+
+    def check_responses(self, qs, responses):
+        "Checks logical constraints on responses."
+        q = sum(qs)
+        qs_check = q == self.Q
+        responses_check = all(
+            [
+                q == sum(classifier_responses)
+                for classifier_responses in responses
+            ]
+        )
+
+        return all([qs_check, responses_check])
---
+++ python/tests/r3/test_r3_alarms.py
@@ -0,0 +1,47 @@
+import ntqr.alarms
+import ntqr.r3.evaluations
+
+import pytest
+
+labels = ("a", "b", "c")
+c1Axioms = ntqr.r3.raxioms.SingleClassifierAxioms(labels, "1")
+c2Axioms = ntqr.r3.raxioms.SingleClassifierAxioms(labels, "2")
+alarm = ntqr.alarms.SingleClassifierAxiomAlarm(
+    18, [c1Axioms, c2Axioms], ntqr.r3.evaluations.SingleClassifierEvaluations
+)
+
+# Better than 50%
+factors = (2, 2, 2)
+qs = (4, 6, 8)
+
+
+@pytest.mark.parametrize(
+    "qs, factors, corrects, satisfies",
+    (
+        (qs, factors, (3, 4, 5), True),
+        (qs, factors, (2, 4, 5), False),
+        (qs, factors, (3, 3, 5), False),
+    ),
+)
+def test_single_classifier_alarm_generate_safety_specification(
+    qs, factors, corrects, satisfies
+):
+    satisfies_safety_specification = alarm.generate_safety_specification(
+        factors
+    )
+    assert satisfies_safety_specification(qs, corrects) == satisfies
+
+
+@pytest.mark.parametrize(
+    "qs, factors, responses, misaligned",
+    (
+        (qs, factors, ((2, 8, 8), (3, 7, 8)), False),
+        (qs, factors, ((10, 8, 0), (0, 8, 10)), True),
+    ),
+)
+def test_misalignment_alarm_at_qs(qs, factors, responses, misaligned):
+    satisfies_safety_spec = alarm.generate_safety_specification(factors)
+    assert (
+        alarm.misaligned_at_qs(satisfies_safety_spec, qs, responses)
+        == misaligned
+    )
